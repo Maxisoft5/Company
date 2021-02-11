7704c67b3d2165c2be61bb56ba9701b004f125f5
maxisoft4@gmail.com <maxisoft4@gmail.com>
2021-02-11 14:47:36 +0200

    Release

---
+++ Company/Company/Extensions/ListExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Company.Models.Abstraction;
+using System.Linq;
+using System.Collections;
+
+namespace Company.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static bool IsExists(this List<Employee> list, Employee item)
+        {
+            return list.Contains(item);
+        }
+
+        public static List<T> GetAllEmployees<T>(this List<T> list)
+        {
+            return list;
+        }
+
+        public static int GetEmployeesCount<T>(this List<T> list)
+        {
+            return list.Count;
+        }
+
+        public static void PrintEmployees(this List<Employee> list)
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} - {list[i].Name} - {list[i].LastName} - {list[i].MiddleName} - Start work date: {list[i].StartWorkDate}");
+            }
+        }
+
+    }
+}
---
+++ Company/Company/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Company.Models.Abstraction;
+
+namespace Company.Models
+{
+    public class Company
+    {
+        public List<Employee> Employees { get; set; }
+
+        public Company()
+        {
+            Employees = new List<Employee>();
+        }
+      
+    }
+}
